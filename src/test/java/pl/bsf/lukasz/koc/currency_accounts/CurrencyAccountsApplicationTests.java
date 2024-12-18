1c30eee768ce3f93cb4ff909b8d5dd691fe40913
Lukasz Koc <lukasz.koc@plus.pl>
2024-12-18 18:30:20 +0100

    initial Commit - mvn pom and properties for: h2, lombock, mapstruct, logger

---
+++ src/test/java/pl/bsf/lukasz/koc/currency_accounts/CurrencyAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bsf.lukasz.koc.currency_accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
